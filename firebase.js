7acca669d42c5eddee27d990720ae95723bd3fcc
YT:ALEXGAMING <alexsanchezfollia@gmail.com>
2026-02-22 20:37:06 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,16 @@
-// firebase.js (en la raíz o en lib/ según tu estructura)
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD13N4-9MjTrmlPwGP7mves0Exje4v2ACw",
-  authDomain: "kahoot-8529e.firebaseapp.com",
-  databaseURL: "https://kahoot-8529e-default-rtdb.firebaseio.com",
-  projectId: "kahoot-8529e",
-  storageBucket: "kahoot-8529e.appspot.com",
-  messagingSenderId: "313414356056",
-  appId: "1:313414356056:web:4aab4587f7df9393008e2d",
-  measurementId: "G-8T4CPC1BQ3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
